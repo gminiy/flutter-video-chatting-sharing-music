b28c3b5e7330ee7434b8cf18332f0209534742f8
gminiy <gmini.y@gmail.com>
2024-01-17 22:20:23 +0900

    fix: auth_repository 를 interface로 변경

--- lib/auth/domain/repository/auth_repository.dart
+++ lib/auth/domain/repository/auth_repository.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_video_chatting_sharing_music/auth/domain/model/user_model.dart';
 
-abstract class AuthRepository {
+abstract interface class AuthRepository {
   Future<bool> isLogin();
   Future<UserModel> login();
   Future<void> logout();
