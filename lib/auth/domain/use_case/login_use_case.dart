cbb7f77f87f6120cd851b596a4457fa24fbcc49f
gminiy <gmini.y@gmail.com>
2024-01-19 15:29:41 +0900

    feat: login_use_case.dart

--- lib/auth/domain/use_case/login_use_case.dart
+++ lib/auth/domain/use_case/login_use_case.dart
@@ -1,5 +1,14 @@
 import 'package:flutter_video_chatting_sharing_music/auth/domain/model/user_model.dart';
+import 'package:flutter_video_chatting_sharing_music/auth/domain/repository/auth_repository.dart';
 
-abstract interface class LoginUseCase {
-  Future<UserModel> execute();
+class LoginUseCase {
+  AuthRepository _repository;
+
+  LoginUseCase({
+    required AuthRepository repository,
+  }) : _repository = repository;
+
+  Future<UserModel> execute() async {
+    return await _repository.login();
+  }
 }
