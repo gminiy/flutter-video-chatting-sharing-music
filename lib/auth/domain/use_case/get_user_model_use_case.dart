e1c5225b00e7170fe8675e0d9a744228baca7902
gminiy <gmini.y@gmail.com>
2024-01-19 15:07:09 +0900

    feat: get_user_model_use_case.dart, interface => class

--- lib/auth/domain/use_case/get_user_model_use_case.dart
+++ lib/auth/domain/use_case/get_user_model_use_case.dart
@@ -1,5 +1,15 @@
 import 'package:flutter_video_chatting_sharing_music/auth/domain/model/user_model.dart';
+import 'package:flutter_video_chatting_sharing_music/auth/domain/repository/auth_repository.dart';
 
-abstract interface class GetUserModelUseCase {
-  Future<UserModel> execute();
+class GetUserModelUseCase {
+  final AuthRepository _repository;
+  UserModel? userModel;
+
+  GetUserModelUseCase({
+    required AuthRepository repository,
+  }) : _repository = repository;
+
+  Future<UserModel> execute() async {
+    return userModel ?? await _repository.getUserModel();
+  }
 }
