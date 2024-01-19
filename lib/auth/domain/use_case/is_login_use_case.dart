4d746bd1d334533dd85aebbdf6de8680c930d581
gminiy <gmini.y@gmail.com>
2024-01-19 15:27:17 +0900

    feat: is_login_use_case.dart

--- lib/auth/domain/use_case/is_login_use_case.dart
+++ lib/auth/domain/use_case/is_login_use_case.dart
@@ -1,3 +1,13 @@
-abstract interface class IsLoginUseCase {
-  Future<bool> execute();
+import 'package:flutter_video_chatting_sharing_music/auth/domain/repository/auth_repository.dart';
+
+class IsLoginUseCase {
+  AuthRepository _repository;
+
+  IsLoginUseCase({
+    required AuthRepository repository,
+  }) : _repository = repository;
+
+  Future<bool> execute() async {
+    return await _repository.isLogin();
+  }
 }
