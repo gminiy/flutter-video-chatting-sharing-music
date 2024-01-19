051876434432375cba29fa0e5a0c67aa5581ad6b
gminiy <gmini.y@gmail.com>
2024-01-19 19:46:56 +0900

    feat: LoginViewModel authStatus 적용

--- lib/auth/presentation/login/login_view_model.dart
+++ lib/auth/presentation/login/login_view_model.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_video_chatting_sharing_music/auth/domain/use_case/login_use_case.dart';
+import 'package:flutter_video_chatting_sharing_music/auth/presentation/auth_status.dart';
 
-class LoginViewModel extends ChangeNotifier {}
+class LoginViewModel extends ChangeNotifier {
+  final LoginUseCase _loginUseCase;
+  final AuthStatus _authStatus;
+
+  LoginViewModel({
+    required LoginUseCase loginUseCase,
+    required AuthStatus authStatus,
+  })
+      : _loginUseCase = loginUseCase,
+        _authStatus = authStatus;
+
+  Future<void> login() async {
+    await _loginUseCase.execute();
+    _authStatus.setIsLogin(true);
+    notifyListeners();
+  }
+}
