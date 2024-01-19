94f3ce223c7b7871694af5a314988e240f81b9fb
gminiy <gmini.y@gmail.com>
2024-01-19 19:47:30 +0900

    feat: router 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_video_chatting_sharing_music/auth/domain/use_case/is_login_use_case.dart';
+import 'package:flutter_video_chatting_sharing_music/auth/presentation/auth_status.dart';
 import 'package:flutter_video_chatting_sharing_music/di/di_setup.dart';
+import 'package:flutter_video_chatting_sharing_music/router/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
   diSetup();
+  final isLogin = await getIt<IsLoginUseCase>().execute();
+  getIt<AuthStatus>().setIsLogin(isLogin);
+
   runApp(const MyApp());
 }
 
@@ -15,7 +21,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
---
+++ lib/router/router.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_video_chatting_sharing_music/auth/presentation/auth_status.dart';
+import 'package:flutter_video_chatting_sharing_music/auth/presentation/login/login_screen.dart';
+import 'package:flutter_video_chatting_sharing_music/auth/presentation/login/login_view_model.dart';
+import 'package:flutter_video_chatting_sharing_music/di/di_setup.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+final router = GoRouter(
+  initialLocation: '/login',
+  routes: [
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => ChangeNotifierProvider(
+        create: (context) => getIt<LoginViewModel>(),
+        child: const LoginScreen(),
+      ),
+    ),
+  ],
+  refreshListenable: getIt<AuthStatus>(),
+  redirect: (context, state) {
+    if (state.fullPath == '/login') {
+      return null;
+    }
+
+    if (!(getIt<AuthStatus>().isLogin)) {
+      return '/login';
+    }
+
+    return null;
+  },
+);
