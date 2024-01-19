b42bec91c90f3eaa8c68335ef0edccd7dd7190f7
gminiy <gmini.y@gmail.com>
2024-01-19 20:20:22 +0900

    fix: initialLocation 수정

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -6,7 +6,7 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 
 final router = GoRouter(
-  initialLocation: '/login',
+  initialLocation: '/',
   routes: [
     GoRoute(
       path: '/login',
