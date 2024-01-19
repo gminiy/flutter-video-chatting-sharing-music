ce0de35464ffa871e222b45ca2da44c62732b08b
gminiy <gmini.y@gmail.com>
2024-01-19 20:17:22 +0900

    feat: login_screen.dart

--- lib/auth/presentation/login/login_screen.dart
+++ lib/auth/presentation/login/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_video_chatting_sharing_music/auth/presentation/login/login_view_model.dart';
+import 'package:provider/provider.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,12 +13,21 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
+    LoginViewModel viewModel = context.watch<LoginViewModel>();
+
     return Scaffold(
       body: Center(
         child: GestureDetector(
           child: Image.asset(
             'assets/kakao/kakao_login_medium_wide.png',
           ),
+          onTap: () async {
+            await viewModel.login();
+
+            if (!mounted) return;
+
+            context.go('/');
+          },
         ),
       ),
     );
